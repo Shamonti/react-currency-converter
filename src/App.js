653b94ae310fde1aaa8606ca6fc64d8c7a798726
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-11-04 13:50:26 +0600

    Fetched data from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect, useState } from 'react';
+
 export default function App() {
+  const [amount, setAmount] = useState(null);
+  const [output, setOutput] = useState(null);
+  useEffect(
+    function () {
+      async function fetchConversion() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=EUR&to=USD`
+        );
+        const data = await res.json();
+        setOutput(data.rates.USD);
+      }
+      fetchConversion();
+    },
+    [amount]
+  );
+
+  const handleChange = function (e) {
+    setAmount(e.target.value);
+  };
+
   return (
     <div>
-      <input type='text' />
+      <input type='text' onChange={handleChange} />
       <select>
         <option value='USD'>USD</option>
         <option value='EUR'>EUR</option>
@@ -16,7 +38,7 @@ export default function App() {
         <option value='CAD'>CAD</option>
         <option value='INR'>INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>OUTPUT: {output}</p>
     </div>
   );
 }
